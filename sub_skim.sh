482603abfc1644204de4971f1c20a0175d5e1d9d
Dener Lemos <denerslemos@gmail.com>
2023-02-13 14:22:08 -0600

    Update sub_skim.sh

--- sub_skim.sh
+++ sub_skim.sh
@@ -3,8 +3,8 @@
 echo "Setup CMSSW (ROOT version)"
 cd /afs/cern.ch/user/d/ddesouza/CMSSW_12_5_0/src
 eval `scramv1 runtime -sh`
-cd /afs/cern.ch/work/d/ddesouza/news/pPbskims
+cd /afs/cern.ch/work/d/ddesouza/news/XeXeskims
 echo "Submit skim jobs at "
 echo PWD: $PWD
 
-./pPbSkim $1 $2 $3
+./XeXeSkim $1 $2 $3
